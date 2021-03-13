9e2213730e25850c43b406ae138a5c2ad1f86f7b
javier890 <79923525+javier890@users.noreply.github.com>
2021-03-13 11:05:54 -0500

    Add files via upload
    tarea

---
+++ JAV (2).py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # REGRESION LINEAL
+# ## INTRODUCCION
+# 
+# La regresión lineal es un método estadístico que trata de modelar la relación entre una variable continua y una o más variables independientes mediante el ajuste de una ecuación lineal. Se llama regresión lineal simple cuando solo hay una variable independiente y regresión lineal múltiple cuando hay más de una. Dependiendo del contexto, a la variable modelada se le conoce como variable dependiente o variable respuesta, y a las variables independientes como regresores, predictores.
+# 
+
+# ## BREVE HISTORIA
+# 
+# La primera forma de regresión lineal documentada fue el método de los mínimos cuadrados que fue publicada por Legendre en 1805, Gauss publicó un trabajo en donde desarrollaba de manera más profunda el método de los mínimos cuadrados,1​ y en dónde se incluía una versión del teorema de Gauss-Márkov.
+# 
+# El término regresión se utilizó por primera vez en el estudio de variables antropométricas: al comparar la estatura de padres e hijos, donde resultó que los hijos cuyos padres tenían una estatura muy superior al valor medio, tendían a igualarse a este, mientras que aquellos cuyos padres eran muy bajos tendían a reducir su diferencia respecto a la estatura media; es decir, "regresaban" al promedio.2​ La constatación empírica de esta propiedad se vio reforzada más tarde con la justificación teórica de ese fenómeno.
+# 
+# El término lineal se emplea para distinguirlo del resto de técnicas de regresión, que emplean modelos basados en cualquier clase de función matemática. Los modelos lineales son una explicación simplificada de la realidad, mucho más ágiles y con un soporte teórico mucho más extenso por parte de la matemática y la estadística.
+# 
+# Pero bien, como se ha dicho, se puede usar el término lineal para distinguir modelos basados en cualquier clase de aplicación.(WIKIPEDIA)
+# I[IMAGEN](https://es.wikipedia.org/wiki/Regresi%C3%B3n_lineal#:~:text=La%20primera%20forma%20de%20regresi%C3%B3n,del%20teorema%20de%20Gauss%2DM%C3%A1rkov.)
+# 
+# 
+
+# ## plantamiento del algoritmo
+# 
+# El modelo de regresión lineal (Legendre, Gauss, Galton y Pearson) considera que, dado un conjunto de observaciones  {yi,xi1,...,xnp}ni=1 , la media  μ  de la variable respuesta  y  se relaciona de forma lineal con la o las variables regresoras  x1  ...  xp  acorde a la ecuación:
+# 
+# μy=β0+β1x1+β2x2+...+βpxp
+#  
+# El resultado de esta ecuación se conoce como la línea de regresión poblacional, y recoge la relación entre los predictores y la media de la variable respuesta.
+# 
+# Otra definición que se encuentra con frecuencia en los libros de estadística es:
+# 
+# yi=β0+β1xi1+β2xi2+...+βpxip+ϵi
+#  
+# En este caso, se está haciendo referencia al valor de  y  para una observación  i  concreta. El valor de una observación puntual nunca va a ser exactamente igual al promedio, de ahí que se añada el término de error  ϵ .
+# 
+# En ambos casos, la interpretación de los elementos del modelo es la misma:
+# 
+# β0 : es la ordenada en el origen, se corresponde con el valor promedio de la variable respuesta  y  cuando todos los predictores son cero.
+# 
+# βj : es el efecto promedio que tiene sobre la variable respuesta el incremento en una unidad de la variable predictora  xj , manteniéndose constantes el resto de variables. Se conocen como coeficientes parciales de regresión.
+# 
+# e : es el residuo o error, la diferencia entre el valor observado y el estimado por el modelo. Recoge el efecto de todas aquellas variables que influyen en  y  pero que no se incluyen en el modelo como predictores.
+# 
+# En la gran mayoría de casos, los valores  β0  y  βj  poblacionales se desconocen, por lo que, a partir de una muestra, se obtienen sus estimaciones  β^0  y  β^j . Ajustar el modelo consiste en estimar, a partir de los datos disponibles, los valores de los coeficientes de regresión que maximizan la verosimilitud (likelihood), es decir, los que dan lugar al modelo que con mayor probabilidad puede haber generado los datos observados.
+# 
+# El método empleado con más frecuencia es el ajuste por mínimos cuadrados ordinarios (OLS), que identifica como mejor modelo la recta (o plano si es regresión múltiple) que minimiza la suma de las desviaciones verticales entre cada dato de entrenamiento y la recta, elevadas al cuadrado.
+
+# El modelo de regresión lineal (Legendre, Gauss, Galton y Pearson) considera que, dado un conjunto de observaciones  {yi,xi1,...,xnp}ni=1 , la media  μ  de la variable respuesta  y  se relaciona de forma lineal con la o las variables regresoras  x1  ...  xp  acorde a la ecuación:
+# 
+# μy=β0+β1x1+β2x2+...+βpxp
+#  
+# El resultado de esta ecuación se conoce como la línea de regresión poblacional, y recoge la relación entre los predictores y la media de la variable respuesta.
+# 
+# Otra definición que se encuentra con frecuencia en los libros de estadística es:
+# 
+# yi=β0+β1xi1+β2xi2+...+βpxip+ϵi
+#  
+# En este caso, se está haciendo referencia al valor de  y  para una observación  i  concreta. El valor de una observación puntual nunca va a ser exactamente igual al promedio, de ahí que se añada el término de error  ϵ .
+# 
+# En ambos casos, la interpretación de los elementos del modelo es la misma:
+# 
+# β0 : es la ordenada en el origen, se corresponde con el valor promedio de la variable respuesta  y  cuando todos los predictores son cero.
+# 
+# βj : es el efecto promedio que tiene sobre la variable respuesta el incremento en una unidad de la variable predictora  xj , manteniéndose constantes el resto de variables. Se conocen como coeficientes parciales de regresión.
+# 
+# e : es el residuo o error, la diferencia entre el valor observado y el estimado por el modelo. Recoge el efecto de todas aquellas variables que influyen en  y  pero que no se incluyen en el modelo como predictores.
+# 
+# En la gran mayoría de casos, los valores  β0  y  βj  poblacionales se desconocen, por lo que, a partir de una muestra, se obtienen sus estimaciones  β^0  y  β^j . Ajustar el modelo consiste en estimar, a partir de los datos disponibles, los valores de los coeficientes de regresión que maximizan la verosimilitud (likelihood), es decir, los que dan lugar al modelo que con mayor probabilidad puede haber generado los datos observados.
+# 
+# El método empleado con más frecuencia es el ajuste por mínimos cuadrados ordinarios (OLS), que identifica como mejor modelo la recta (o plano si es regresión múltiple) que minimiza la suma de las desviaciones verticales entre cada dato de entrenamiento y la recta, elevadas al cuadrado.
+# 
+
+# ## Supóngase que un analista de deportes quiere saber si existe una relación entre el número de veces que batean los jugadores de un equipo de béisbol y el número de runs que consigue. En caso de existir y de establecer un modelo, podría predecir el resultado del partido
+
+# El primer paso antes de generar un modelo de regresión simple es representar los datos para poder intuir si existe una relación y cuantificar dicha relación mediante un coeficiente de correlación.
+
+# ## Algoritmo
+
+# 
+# 
+
+# In[ ]:
+
+
+
+
+
+# In[12]:
+
+
+# Tratamiento de datos
+# ==============================================================================
+import pandas as pd
+import numpy as np
+
+# Gráficos
+# ==============================================================================
+import matplotlib.pyplot as plt
+from matplotlib import style
+import seaborn as sns
+
+# Preprocesado y modelado
+# ==============================================================================
+from scipy.stats import pearsonr
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
+# Configuración matplotlib
+# ==============================================================================
+plt.rcParams['image.cmap'] = "bwr"
+#plt.rcParams['figure.dpi'] = "100"
+plt.rcParams['savefig.bbox'] = "tight"
+style.use('ggplot') or plt.style.use('ggplot')
+
+# Configuración warnings
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# ## datos
+# 
+
+# In[ ]:
+
+
+# Datos
+# ==============================================================================
+equipos = ["Texas","Boston","Detroit","Kansas","St.","New_S.","New_Y.",
+           "Milwaukee","Colorado","Houston","Baltimore","Los_An.","Chicago",
+           "Cincinnati","Los_P.","Philadelphia","Chicago","Cleveland","Arizona",
+           "Toronto","Minnesota","Florida","Pittsburgh","Oakland","Tampa",
+           "Atlanta","Washington","San.F","San.I","Seattle"]
+bateos = [5659,  5710, 5563, 5672, 5532, 5600, 5518, 5447, 5544, 5598,
+          5585, 5436, 5549, 5612, 5513, 5579, 5502, 5509, 5421, 5559,
+          5487, 5508, 5421, 5452, 5436, 5528, 5441, 5486, 5417, 5421]
+
+runs = [855, 875, 787, 730, 762, 718, 867, 721, 735, 615, 708, 644, 654, 735,
+        667, 713, 654, 704, 731, 743, 619, 625, 610, 645, 707, 641, 624, 570,
+        593, 556]
+
+datos = pd.DataFrame({'equipos': equipos, 'bateos': bateos, 'runs': runs})
+
+datos.head(3)
+
+
+# 
+# equipos	bateos	runs
+# 0	Texas	5659	855
+# 1	Boston	5710	875
+# 2	Detroit	5563	787
+# 
+
+# ## grafico
+# 
+
+# In[ ]:
+
+
+# Gráfico
+# ==============================================================================
+fig, ax = plt.subplots(figsize=(6, 3.84))
+
+datos.plot(
+    x    = 'bateos',
+    y    = 'runs',
+    c    = 'firebrick',
+    kind = "scatter",
+    ax   = ax
+)
+ax.set_title('Distribución de bateos y runs');
+
